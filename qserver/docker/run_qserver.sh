6659707fc840e9402fc26205a28fc10f9fd1053b
Arun A <arun.mudhaliar@gmail.com>
2023-10-29 23:37:32 +0530

    ref added

--- qserver/docker/run_qserver.sh
+++ qserver/docker/run_qserver.sh
@@ -1,5 +1,6 @@
 pwd
 echo "This script only works in linux. 'ip' cmd not available in other OS."
+#https://unix.stackexchange.com/questions/8518/how-to-get-my-own-ip-address-and-save-it-to-a-variable-in-a-shell-script
 cd ./qserver
 ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
 port=4000
