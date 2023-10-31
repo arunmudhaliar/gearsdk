30f2db6c422fd0a23c3101948c39518dbe41a147
Arun A <arun.mudhaliar@gmail.com>
2023-10-31 14:43:19 +0530

    docker file refactor for qh3

---
+++ qh3server/docker/prepare-docker-qh3server-folder.sh
@@ -0,0 +1,56 @@
+cd ../../
+rm -rf ./docker-qh3server
+
+mkdir ./docker-qh3server
+cp -r ./common ./docker-qh3server/common
+cp -r ./networkcommon/ ./docker-qh3server/networkcommon
+cp -r ./qh3server/ ./docker-qh3server/qh3server
+cp ./qh3server/docker/Dockerfile ./docker-qh3server/Dockerfile
+cp ./qh3server/docker/make_qh3server.sh ./docker-qh3server/make_qh3server.sh
+cp ./qh3server/docker/run_qh3server.sh ./docker-qh3server/run_qh3server.sh
+
+# docker-qh3server
+rm -rf -- ./docker-qh3server/**/*.o
+rm -rf -- ./docker-qh3server/**/**/*.o
+rm -rf -- ./docker-qh3server/**/**/**/*.o
+rm -rf -- ./docker-qh3server/**/**/**/**/*.o
+
+rm -rf -- ./docker-qh3server/**/.DS_Store
+rm -rf -- ./docker-qh3server/**/**/.DS_Store
+rm -rf -- ./docker-qh3server/**/**/**/.DS_Store
+rm -rf -- ./docker-qh3server/**/**/**/**/.DS_Store
+
+rm -rf -- ./docker-qh3server/**/DerivedData
+rm -rf -- ./docker-qh3server/**/**/DerivedData
+rm -rf -- ./docker-qh3server/**/**/**/DerivedData
+rm -rf -- ./docker-qh3server/**/**/**/**/DerivedData
+
+rm -rf -- ./docker-qh3server/**/*.xcodeproj
+rm -rf -- ./docker-qh3server/**/**/*.xcodeproj
+rm -rf -- ./docker-qh3server/**/**/**/*.xcodeproj
+rm -rf -- ./docker-qh3server/**/**/**/**/*.xcodeproj
+
+rm -rf ./docker-qh3server/networkcommon/libs/macos
+rm -rf ./docker-qh3server/qh3server/docker
+rm -rf ./docker-qh3server/qh3server/*.dSYM
+rm -rf ./docker-qh3server/qh3server/qh3server-app
+
+# build
+echo "use 'sudo docker build -t qh3server-exp .' to build the docker"
+# sudo docker build -t qh3server.
+
+# run
+echo "use 'sudo docker run --name qh3server-container -d qh3server-exp' to run the docker"
+# sudo docker run --name qh3server-container -d qh3server-exp
+
+# stop
+echo "use 'sudo docker stop qh3server-container' to stop the docker"
+# sudo docker stop qh3server-container
+
+# restart
+echo "use 'sudo docker restart qh3server-container' to re-start the docker"
+# sudo docker restart qh3server-container
+
+# REMOVE
+echo "use 'sudo docker rm --force qh3server-container' to REMOVE the docker"
+# sudo docker rm --force qh3server-container
--- qh3server/docker/run_qh3server.sh
+++ qh3server/docker/run_qh3server.sh
@@ -3,5 +3,5 @@ echo "This script only works in linux. 'ip' cmd not available in other OS."
 #https://unix.stackexchange.com/questions/8518/how-to-get-my-own-ip-address-and-save-it-to-a-variable-in-a-shell-script
 cd ./qh3server
 ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
-port=4000
+port=4004
 ./qh3server-app $ip $port
