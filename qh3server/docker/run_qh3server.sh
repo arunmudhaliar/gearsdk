567659b58c61f3b43bbd858b31cdf6f7b6c25645
Arun A <arun.mudhaliar@gmail.com>
2023-11-07 00:15:59 +0530

    missing prev commit

--- qh3server/docker/run_qh3server.sh
+++ qh3server/docker/run_qh3server.sh
@@ -4,4 +4,5 @@ echo "This script only works in linux. 'ip' cmd not available in other OS."
 cd ./qh3server
 ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
 port=4004
-./qh3server-app $ip $port
+db_uri=mongodb://192.168.0.230:6006
+./qh3server-app --h $ip --p $port --db $db_uri
