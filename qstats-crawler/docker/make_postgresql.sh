44ca8e5a665a4ac4639298ac3994c3eb72bd7c52
Arun A <arun.mudhaliar@gmail.com>
2023-11-28 03:00:54 +0530

    test

---
+++ qstats-crawler/docker/make_postgresql.sh
@@ -0,0 +1,11 @@
+cd ./postgresql-15.5
+RUN mkdir ./build
+RUN ./configure --prefix=/app/postgresql-15.5/build
+make clean
+make release
+make install
+cd ..
+rm -r ./qstats-crawler/libs/linux
+mkdir ./qstats-crawler/libs/linux
+cp -a /app/postgresql-15.5/build/lib/. ./qstats-crawler/libs/linux
+ls ./qstats-crawler/libs/linux/
