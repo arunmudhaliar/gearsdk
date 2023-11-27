91918c5cbba26955d73e5d20b205254308c4dca6
Arun A <arun.mudhaliar@gmail.com>
2023-11-28 03:21:02 +0530

    test

--- qstats-crawler/docker/make_postgresql.sh
+++ qstats-crawler/docker/make_postgresql.sh
@@ -1,6 +1,7 @@
 cd ./postgresql-15.5
 mkdir ./build
-./configure --prefix=/app/postgresql-15.5/build
+# ./configure --prefix=/app/postgresql-15.5/build
+./configure
 make clean
 make release
 make install
@@ -8,4 +9,7 @@ cd ..
 rm -r ./qstats-crawler/libs/linux
 mkdir ./qstats-crawler/libs/linux
 cp -a /app/postgresql-15.5/build/lib/. ./qstats-crawler/libs/linux
+echo "FINISHED MAKE-POSTGRESQL"
+echo "FINISHED MAKE-POSTGRESQL"
+echo "FINISHED MAKE-POSTGRESQL"
 ls ./qstats-crawler/libs/linux/
