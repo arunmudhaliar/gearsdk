378022a2f693b3c05dbce6dd7411b4cfda342b96
Arun A <arun.mudhaliar@gmail.com>
2023-11-28 11:09:47 +0530

    test

--- qstats-crawler/qstats-crawler/main.cpp
+++ qstats-crawler/qstats-crawler/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, const char * argv[]) {
     UNUSED(argc);
     UNUSED(argv);
     
+#if 0
     fs::path root_file_path = "./stats/qh3_statfile";
     
     if (argc%2<2) {
@@ -36,5 +37,18 @@ int main(int argc, const char * argv[]) {
     qstats_crawler crawler;
     crawler.try_crawl(root_file_path.c_str());
     DEBUG_PRINT(LOG_LEVEL_0, __LOGTAG__, "qstats crawler finished...");
+#endif
+    
+    qtimer_sceduler scheduler;
+    struct ev_loop* loop = ev_default_loop(0);
+    ev_tstamp creation_time = ev_now(loop);
+    scheduler.set_ev_lopp(loop);
+    
+    qtimer* keep_alive_loop = scheduler.schedule_repeat_timer([loop, creation_time](qtimer& timer) {
+        UNUSED(timer);
+        DEBUG_PRINT_IMPORTANT(__LOGTAG__, "alive - t:%5.2fs", ev_now(loop) - creation_time);
+    }, 600);
+    UNUSED(keep_alive_loop);
+    ev_run(loop, 0);
     return 0;
 }
