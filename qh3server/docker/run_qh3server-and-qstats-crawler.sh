b34a90c78ae0b1e3bc179819b380ac0f296051eb
Arun A <arun.mudhaliar@gmail.com>
2023-12-14 23:30:58 +0530

    test

--- qh3server/docker/run_qh3server-and-qstats-crawler.sh
+++ qh3server/docker/run_qh3server-and-qstats-crawler.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 pwd
 echo "This script only works in linux. 'ip' cmd not available in other OS."
 #https://unix.stackexchange.com/questions/8518/how-to-get-my-own-ip-address-and-save-it-to-a-variable-in-a-shell-script
@@ -7,11 +8,11 @@ port=4004
 db_uri=mongodb://192.168.0.230:6006
 redis_ip=192.168.0.230
 redis_port=6379
-./qh3server-app --h $ip --p $port --db $db_uri --rh $redis_ip --rp $redis_port
-../qstats-crawler/qstats-crawler-app --f "./stats/qh3_statfile"
+./qh3server-app --h $ip --p $port --db $db_uri --rh $redis_ip --rp $redis_port &
+../qstats-crawler/qstats-crawler-app --f "./stats/qh3_statfile" &
 
 # # Wait for any process to exit
-# wait -n
+wait -n
 
 # # Exit with status of process that exited first
-# exit $?
+exit $?
