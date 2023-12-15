0a7aeca1a9552be85333fcfd60390eb3d58fbff8
Arun A <arun.mudhaliar@gmail.com>
2023-12-15 16:40:50 +0530

    test

--- qstats-crawler/qstats-crawler/main.cpp
+++ qstats-crawler/qstats-crawler/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, const char * argv[]) {
     fs::path root_file_path = "./stats/qh3_statfile";
     
     if (argc%2<2) {
-        DEBUG_PRINT_ERROR(__LOGTAG__, "Failed to resolve arguments !!!. Using default path %s", root_file_path.c_str());
+        DEBUG_PRINT_ERROR(__LOGTAG__, "Failed to resolve arguments !!!. Using default path %s, argc %d", root_file_path.c_str(), argc);
         DEBUG_PRINT_IMPORTANT2(__LOGTAG__, "Usage : <executable> '--f <root file name>'");
     } else {
         // default to root
