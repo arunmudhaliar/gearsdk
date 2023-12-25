4925713ac33f61131857e817d0c8a21f5591e558
Arun A <arun.mudhaliar@gmail.com>
2023-12-25 15:13:39 +0530

    test

--- qh3server/qh3server/main.cpp
+++ qh3server/qh3server/main.cpp
@@ -33,12 +33,12 @@ int main(int argc, const char* argv[]) {
         exit(0);
     }
     
-    http3_sample_server server(mongodb_uri.c_str(), redis_ip.c_str(), redis_port);
-    server.run(host, port, rootDir, nullptr);
+//    http3_sample_server server(mongodb_uri.c_str(), redis_ip.c_str(), redis_port);
+//    server.run(host, port, rootDir, nullptr);
     
-//    router_config config(host, port, mongodb_uri, redis_ip, redis_port, rootDir, nullptr);
-//    qh3simple_router router(config);
-//    router.run();
+    router_config config(host, port, mongodb_uri, redis_ip, redis_port, rootDir, nullptr);
+    qh3simple_router router(config);
+    router.run();
     
     //    server.test_mongo_db();
 
