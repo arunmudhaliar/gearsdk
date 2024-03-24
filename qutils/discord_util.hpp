e1cf92b842e722bcf734f2ede9d5491e493b934e
Arun A <arun.mudhaliar@gmail.com>
2024-03-24 21:41:48 +0530

    clang-format - qutils

--- qutils/discord_util.hpp
+++ qutils/discord_util.hpp
@@ -15,24 +15,26 @@
 #define __LOGTAG__ "discord_util"
 
 class discord_util {
-public:
-    struct discord_async_data {
-    private:
-        discord_async_data(){};
-    public:
-        discord_async_data(const qstring& msg) : msg(msg) {
-        }
-        pthread_t tid;
-        qstring msg;
-    };
-    
-    static void set_web_hook(const qstring& web_hook);
-    static int send(const qstring& msg);
-    static void send_async(const qstring& msg);
-    
-private:
-    static void* send_async_internal(void* data);
-    static qstring current_web_hook;
+  public:
+	struct discord_async_data {
+	  private:
+		discord_async_data() {};
+
+	  public:
+		discord_async_data(const qstring& msg)
+			: msg(msg) {
+		}
+		pthread_t tid;
+		qstring msg;
+	};
+
+	static void set_web_hook(const qstring& web_hook);
+	static int send(const qstring& msg);
+	static void send_async(const qstring& msg);
+
+  private:
+	static void* send_async_internal(void* data);
+	static qstring current_web_hook;
 };
 
 #endif /* discord_util_hpp */
