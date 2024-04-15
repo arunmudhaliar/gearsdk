8e05604e9515e9bf2a24961c5462058c26c414ca
Arun A <arun.mudhaliar@gmail.com>
2024-04-16 00:35:17 +0530

    fixing to relative path for qunitysdk symlink

--- create_sym_links.sh
+++ create_sym_links.sh
@@ -55,12 +55,13 @@ cd ../../
 echo 'creating qunitysdk symlink'
 rm -rf ./qunityplugin/qunityplugin_unity_proj/Assets/Plugins/qunitysdk
 cd ./qunityplugin/qunityplugin_unity_proj
+cd ./Assets/Plugins
+ln -s ./../../qunitysdk_module/source ./qunitysdk
+rm -rf ./qunitysdk/libs
+mkdir ./qunitysdk/libs
+cd ./qunitysdk/libs
+ln -s ./../../../qunitysdk_module/libs/android ./android
+ln -s ./../../../qunitysdk_module/libs/macos_$arch ./macos
+cd ../../../../../../
 # echo $(pwd)
-ln -s $(pwd)/qunitysdk/source $(pwd)/Assets/Plugins/qunitysdk
-rm -rf $(pwd)/Assets/Plugins/qunitysdk/libs
-mkdir $(pwd)/Assets/Plugins/qunitysdk/libs
-ln -s $(pwd)/qunitysdk/libs/android $(pwd)/Assets/Plugins/qunitysdk/libs/android
-ln -s $(pwd)/qunitysdk/libs/macos_$arch $(pwd)/Assets/Plugins/qunitysdk/libs/macos
-cd ../../
-
 echo 'symlink script finished.'
