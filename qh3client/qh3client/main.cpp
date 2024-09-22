4d8dc875f9984c8f729e5fe69c4633bfbeaf8a91
Arun A <arun.mudhaliar@gmail.com>
2024-09-22 22:49:14 +0530

    added -DNDEBUG to release configuration

--- qh3client/qh3client/main.cpp
+++ qh3client/qh3client/main.cpp
@@ -9,14 +9,16 @@
 #include "http3_sample_client.hpp"
 
 #define DEFAULT_REQUEST_IP "127.0.0.1:4004"
+#define DEFAULT_REQUEST_COUNT 10
 
-typedef struct {
+typedef struct app_state_t {
 	qstring host;
 	qstring port;
 	struct ev_loop* loop;
 	http3_sample_client client;
 	qtimer_scheduler scheduler;
 	ev_tstamp creation_time;
+	int request_count = DEFAULT_REQUEST_COUNT;
 } app_state_t;
 
 app_state_t app_state;
@@ -25,6 +27,7 @@ void parse_arguments(int argc, const char* argv[]) {
 	// server address
 	app_state.host = "127.0.0.1";
 	app_state.port = "4004";
+	app_state.request_count = DEFAULT_REQUEST_COUNT;
 
 	qstring request_ip(DEFAULT_REQUEST_IP);
 	std::vector<qstring> request_ip_parts;
@@ -48,11 +51,29 @@ void parse_arguments(int argc, const char* argv[]) {
 			} else {
 				debug_print_error(__LOGTAG__, "Invalid request IP: %s. Defaulting to %s", request_ip.c_str(), DEFAULT_REQUEST_IP);
 			}
+		} else if (strcmp(argv[i], "--requests") == 0 && i + 1 < argc) {
+			app_state.request_count = atoi(argv[++i]);
 		}
 	}
 }
 
+void print_usage(const char* program_name) {
+    fprintf(stderr, "Usage: %s [options]\n\n", program_name);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "  --server <host:port>      Server address in the form of host:port (default: %s)\n", DEFAULT_REQUEST_IP);
+    fprintf(stderr, "  --requests <count>        [optional] No of requests (default: %d)\n\n", DEFAULT_REQUEST_COUNT);
+    fprintf(stderr, "Example:\n");
+    fprintf(stderr, "  %s --server 127.0.0.1:4004 --requests 10\n", program_name);
+}
+
 int main(int argc, const char* argv[]) {
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "--help") == 0) {
+			print_usage(argv[0]);
+			return 0;
+		}
+	}
+
 	parse_arguments(argc, argv);
 	init_gsdk();
 
@@ -65,7 +86,7 @@ int main(int argc, const char* argv[]) {
 
 	app_state.client.set_server_info(app_state.host, app_state.port);
 	app_state.client.set_loop(app_state.loop);
-	app_state.client.create_connections(60);
+	app_state.client.create_connections(app_state.request_count);
 
 	app_state.creation_time = ev_now(app_state.loop);
 	app_state.scheduler.set_loop(app_state.loop);
